77c0b54e3fb71cf4851be7fce08285b7a5b9162b
zgicg <280724396@qq.com>
2026-01-19 01:34:31 -0800

    init

---
+++ TestBuild/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestBuild
+//
+//  Created by bin on 19.1.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestBuild/TestBuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBuildApp.swift
+//  TestBuild
+//
+//  Created by bin on 19.1.26.
+//
+
+import SwiftUI
+
+@main
+struct TestBuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
